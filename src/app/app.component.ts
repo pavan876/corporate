b05cb86a1031c23b5c682837d75cc054e042ba9d
SreekarKondaveeti <sreekar@privme.com>
2022-02-02 13:11:38 +0530

    BASE_URL added
    BASE_URL added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'optimhire-corporate';
-  
+  BASE_URL:String = environment.BASE_URL;
 }
